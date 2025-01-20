32a95dd83d4b63abcf0ef729d8f825fc45e5e522
rydeblack <ryhad.boughanmi@gmail.com>
2025-01-20 15:08:09 +0100

    feat: add scraping tool to get dico words

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import re
+import string
 
 my_words = []
 
@@ -12,6 +13,19 @@ def print_words():
                 print(myword)
             
     print(my_words)
-    
-    
-print_words()
+
+current_index = 0
+
+def get_letter():
+    global current_index
+    letters = list(string.ascii_lowercase)
+    if current_index < len(letters):
+        letter = letters[current_index]
+        current_index += 1
+        return letter
+    return None   
+        
+        
+if __name__ == "__main__":
+    # print_words()
+    get_letter()
