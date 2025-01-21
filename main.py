f071e2944db7cb426ac0bfa44e088e8d121de22e
rydeblack <ryhad.boughanmi@gmail.com>
2025-01-21 11:21:59 +0100

    feat: add shuffle word and hints functions

--- main.py
+++ main.py
@@ -11,16 +11,35 @@ def store_words():
             myword = re.sub('\n', '', myword)
             my_words.append(myword)
             
-            if not "r" in myword:
-                print(myword)
+            # if not "r" in myword:
+                # print(myword)
                   
-    print(my_words) 
+    # print(my_words) 
 
 def shuffle_words():
+    global chosen_word
+    random.shuffle(my_words)
+    chosen_word = my_words[0]
+    
+    print(chosen_word)
+    return chosen_word
+
+  
+def dislay_hints():
+    length_hint = len(chosen_word)
+    print("_" * length_hint)
+    return chosen_word
+    
+    
+def wrong_guess():
     pass
+      
         
 def main():
-    pass
+    store_words()
+    shuffle_words()
+    dislay_hints()
+
         
 if __name__ == "__main__":
-    store_words()
+    main()
